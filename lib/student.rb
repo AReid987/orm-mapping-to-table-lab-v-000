63d95c336dee8744a54162c372fd0cfc38f1e7a6
Antonio Reid <read.musik@gmail.com>
2018-03-09 03:45:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?,?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade).flatten
+    #DB[:conn].execute(sql, self.name, self.grade).flatten
     #binding.pry
   end
   # Remember, you can access your database connection anywhere in this class
